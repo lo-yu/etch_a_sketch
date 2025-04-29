99481bc0069f53e10b13e94d47cc63d618c41e10
Lobsang Yungdung <[10lobsangyungdung10@gmail.com]>
2025-04-29 21:44:43 +0530

    Create a grid

--- script.js
+++ script.js
@@ -2,11 +2,39 @@ let container = document.querySelector("#container");
 
 container.style.display = "flex";
 container.style.border = "solid";
-container.style.height = "500px";
-container.style.width = "500px";
+container.style.height = "650px";
+container.style.width = "650px";
+container.style.flexDirection = "column";
+
+let no = 20;
 
 
-   // let grid = container.createElement("div");
-    //grid.style.border = "solid";
-    //grid.style.color = "blue";
+
+
+
+for( i = 1; i <= no; i++){
+    let div = document.createElement("div");
+    div.setAttribute("id", "column");
+    div.style.flex = "1";
+    container.appendChild(div);
+
+}
+
+let column = document.querySelectorAll("#column");
+
+
+for( i = 0; i <= column.length; i++){
+   column[i].style.display = "flex";
+   for(j = 1; j <= no; j++){
+    let div = document.createElement("div");
+    div.setAttribute("id", "grid");
+    div.style.border = "solid";
+    div.style.flex = "1";
+    column[i].appendChild(div);
+
+
+   }
+}
+
+
 
