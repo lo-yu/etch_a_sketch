7c50f1cb439a19f67d75e7f7b8c724ce13d83653
Lobsang Yungdung <[10lobsangyungdung10@gmail.com]>
2025-04-29 16:11:32 +0530

    Create container to store grid

---
+++ script.js
@@ -0,0 +1,12 @@
+let container = document.querySelector("#container");
+
+container.style.display = "flex";
+container.style.border = "solid";
+container.style.height = "500px";
+container.style.width = "500px";
+
+
+   // let grid = container.createElement("div");
+    //grid.style.border = "solid";
+    //grid.style.color = "blue";
+
