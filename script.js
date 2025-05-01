74d46a048150e24b466aff34d038dab035a5996b
Lobsang Yungdung <[10lobsangyungdung10@gmail.com]>
2025-05-01 11:11:50 +0530

    Add hover effect

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ container.style.height = "650px";
 container.style.width = "650px";
 container.style.flexDirection = "column";
 
-let no = 20;
+let no = 16;
 
 
-
+/*container.addEventListener("mouseover", () => {
+   container.style.backgroundColor = "blue";
+})*/
 
 
 for( i = 1; i <= no; i++){
@@ -28,13 +30,27 @@ for( i = 0; i <= column.length; i++){
    for(j = 1; j <= no; j++){
     let div = document.createElement("div");
     div.setAttribute("id", "grid");
-    div.style.border = "solid";
+    div.classList.add("box");
+    div.style.border = "solid 0.6px";
     div.style.flex = "1";
+    div.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "grey";
+    });
     column[i].appendChild(div);
 
 
    }
 }
+
+/*let grid = document.querySelectorAll("#grid");
+
+for(i = 0; i < grid.length; i++){
+   grid[i].style.backgroundColor = "red";
+   
+}*/
+
+
+
 
 
 
