56d0fe7174036af6e1b8cff139121a4620f1e54b
Lobsang Yungdung <[10lobsangyungdung10@gmail.com]>
2025-05-03 12:47:13 +0530

    Clean the code

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-let container = document.querySelector("#container");
 let column = document.querySelectorAll("#column");
 
-container.style.display = "flex";
-container.style.border = "solid";
-container.style.height = "650px";
-container.style.width = "650px";
-container.style.flexDirection = "column";
-
-//let no = 16;
-
-
 let resizeGrid = function(squares){
-
 
    for( i = 1; i <= squares; i++){
       let div = document.createElement("div");
       div.setAttribute("id", "column");
       div.style.flex = "1";
       container.appendChild(div);
-  
   }
 
   let column = document.querySelectorAll("#column");
@@ -29,19 +17,16 @@ let resizeGrid = function(squares){
     for(j = 1; j <= squares; j++){
       let div = document.createElement("div");
       div.setAttribute("id", "grid");
-      div.classList.add("box");
-      div.style.border = "solid 0.01px";
+      div.style.border = "solid 1px";
       div.style.flex = "1";
       div.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = "Brown";
-      });
+        e.target.style.backgroundColor = "lightGreen";
+      })
       column[i].appendChild(div);
-  
      }
   }
-
-
 }
+
 
 
 let askResize = function(){
@@ -61,13 +46,6 @@ button.addEventListener("click", askResize);
 
 resizeGrid(16);
 
-
-/*let grid = document.querySelectorAll("#grid");
-
-for(i = 0; i < grid.length; i++){
-   grid[i].style.backgroundColor = "red";
-   
-}*/
 
 
 
